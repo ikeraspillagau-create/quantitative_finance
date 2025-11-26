1d8a58dc199224e3e66aac3139357fe4f87e9e18
ikeraspillagau-create <ikeraspillagau@gmail.com>
2025-11-26 13:05:57 +0000

    rama actualizada

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from modules.financials_functions import portfolio_volatility, portfolio_returns
+
+if __name__ == '__main__': 
+    #datos del portafolio
+    tickers = ['IEF', 'SPTL', 'TLT', 'VGLT']
+    start = '2023-01-01'
+    end = '2024-12-31'
+    
+    #descargar retornos del portafolio
+    df = portfolio_returns(
+        tickers=tickers,
+        start=start,
+        end=end
+        )
+    #print(df.head(5))
+    
+    #calculo volatilidad
+    vector_w = np.array([1/len(tickers)] * len(tickers))
+    sigma = portfolio_volatility(df=df, vector_w=vector_w)
+    print(sigma)
+    
---
+++ modules/financials_functions.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np 
+import scipy.stats as stats
+from .backend import market_prices
+
+
+def portfolio_volatility(
+        df:pd.DataFrame, 
+        vector_w:np.array
+        ) -> float:
+    '''
+    Calculo de la volatilidad de un portafolio 
+    de inversiones.
+    
+    - df (pd.DataFrame)
+    Dataframe de retornos del portafolio.
+    vector_w (np.array)
+    vector de pesos de los instrumentos del portafolio
+    
+    Return (float): volatilidad del portafolio
+    '''
+    
+    #matrix varianza covarianza
+    m_cov = df.cov()
+    
+    #vector traspuesto
+    vector_w_t = np.array([vector_w])
+    
+    #varianza
+    vector_cov = np.dot(m_cov,vector_w)
+    varianza = np.dot(vector_w_t,vector_cov)
+    
+    #volatilidad
+    vol = np.sqrt(varianza)
+    
+    return vol
+
+def portfolio_returns(
+    tickers: list,
+    start: str,
+    end: str
+    ) -> pd.DataFrame: 
+    '''
+   Descarga desde la base de datos los precios
+   de los instrumentos indicados en el rango de fechas
+   
+   ticker (list):
+   lista de nemos que componen el portafolio
+   
+   start (str): 
+   fecha de inicio de precios
+   
+   end(str): 
+   fecha de termino de precios
+   
+   Return: (pd.DataFrame): DataFrame de retornos diarios 
+    '''
+    
+    #descargar precios
+    df = market_prices(
+        start_date=start,
+        end_date=end,
+        tickers=tickers
+        )
+    
+    #pivot retornos
+    df_pivot = pd.pivot_table(
+    data=df,
+    index='FECHA',
+    columns='TICKER',
+    values='PRECIO_CIERRE',
+    aggfunc='max'
+    )
+    
+    df_pivot = df_pivot.pct_change().dropna()
+    
+    return df_pivot
+    
+    
+    
